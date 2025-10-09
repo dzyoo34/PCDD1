dbc23da73a70e6420308a57ce7b5a75291842564
Nichita Stepanovici <nichita.stepanovici@iis.utm.md>
2025-10-09 13:51:37 +0300

    изменение под интерфейс Runnable Th1

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 import java.util.*;
 
-class Th1 extends Thread {
+// Класс для первого потока - через интерфейс Runnable
+class Th1 implements Runnable {
     private int[] mas;
+    private String name;
 
-    public Th1(int[] mas) {
+    public Th1(int[] mas, String name) {
         this.mas = mas;
+        this.name = name;
     }
 
     public void run() {
-        System.out.println(getName() + " начало");
+        System.out.println(name + " начало");
         int i = 0;
-
 
         while(i < mas.length) {
             if(mas[i] % 2 == 0) {
@@ -21,8 +23,7 @@ class Th1 extends Thread {
                         int pos2 = i;
                         int suma = pos1 + pos2;
 
-
-                        System.out.println(getName() + ": позиция1=" + pos1 +
+                        System.out.println(name + ": позиция1=" + pos1 +
                                 ", позиция2=" + pos2 +
                                 ", сумма позиций=" + suma +
                                 ", значения: [" + mas[pos1] + ", " + mas[pos2] + "]");
@@ -34,91 +35,6 @@ class Th1 extends Thread {
             i++;
         }
 
-        System.out.println(getName() + " конец");
-    }
-}
-
-class Th2 extends Thread {
-    private int[] mas;
-
-
-    public Th2(int[] mas) {
-        this.mas = mas;
-    }
-
-    public void run() {
-        System.out.println(getName() + " начало");
-
-        int i = mas.length - 1;
-
-        while(i >= 0) {
-            if(mas[i] % 2 == 0) {
-                int pos1 = i;
-                i--;
-                while(i >= 0) {
-                    if(mas[i] % 2 == 0) {
-                        int pos2 = i;
-                        int suma = pos1 + pos2;
-
-
-                        System.out.println(getName() + ": позиция1=" + pos1 +
-                                ", позиция2=" + pos2 +
-                                ", сумма позиций=" + suma +
-                                ", значения: [" + mas[pos1] + ", " + mas[pos2] + "]");
-                        break;
-                    }
-                    i--;
-                }
-            }
-            i--;
-        }
-
-        System.out.println(getName() + " конец");
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        int[] mas = new int[100];
-
-        System.out.println("Массив данных:");
-        for(int i = 0; i < 100; i++) {
-            mas[i] = (int)(Math.random() * 100) + 1;
-            System.out.printf("%3d ", mas[i]);
-
-            if((i + 1) % 20 == 0) {
-                System.out.println();
-            }
-        }
-        System.out.println("\n");
-
-        Th1 thread1 = new Th1(mas);
-        Th2 thread2 = new Th2(mas);
-
-        thread1.setName("Поток-1");
-        thread2.setName("Поток-2");
-
-        thread1.start();
-        thread2.start();
-
-        try {
-            thread1.join();
-            thread2.join();
-        } catch(InterruptedException e) {
-            System.out.println("Ошибка при ожидании потоков");
-        }
-
-        System.out.println("\n========================================");
-        String info = "Лабораторная работа выполнена студентами группы:Степанович Никита и Згурский Дмитрий";
-        for(int i = 0; i < info.length(); i++) {
-            System.out.print(info.charAt(i));
-            try {
-                Thread.sleep(100);
-            } catch(InterruptedException e) {
-                System.out.println("Ошибка при задержке");
-            }
-        }
-
-        System.out.println("\n========================================");
+        System.out.println(name + " конец");
     }
 }
