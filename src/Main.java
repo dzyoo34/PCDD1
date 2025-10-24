4344546aa91143c4f5a0bf703cde7af9b0f9ca34
Nichita Stepanovici <nichita.stepanovici@iis.utm.md>
2025-10-25 01:35:11 +0300

    total peredelka 4-5

--- src/Main.java
+++ src/Main.java
@@ -1,105 +1,73 @@
-import java.util.stream.Stream;
-
-public class Main {
-
-    public static void main(String[] args) {
-        runProgram();
+class Custom_thread extends Thread {
+    Custom_thread(ThreadGroup group, String name, int priority) {
+        super(group, name);
+        setPriority(priority);
+        setDaemon(true);
     }
 
-    private static void runProgram() {
-        Runnable dummyTask = () -> {
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException ignored) {
-                Thread.currentThread().interrupt();
-            }
-        };
-
-        ThreadGroup mainGroup = Thread.currentThread().getThreadGroup();
-
-
-        ThreadGroup G2 = new ThreadGroup("G2");
-        ThreadGroup G3 = new ThreadGroup("G3");
-
-        ThreadGroup G4 = new ThreadGroup(G2, "G4");
-
-        ThreadGroup G1 = new ThreadGroup(G4, "G1");
-
-
-        System.out.println("Создание потоков...");
-
-
-        Thread Tha = new Thread(G1, dummyTask, "Tha"); Tha.setPriority(1); Tha.start();
-        Thread Thb = new Thread(G1, dummyTask, "Thb"); Thb.setPriority(3); Thb.start();
-        Thread Thc = new Thread(G1, dummyTask, "Thc"); Thc.setPriority(8); Thc.start();
-        Thread Thd = new Thread(G1, dummyTask, "Thd"); Thd.setPriority(3); Thd.start();
-
-
-        Thread ThA = new Thread(G4, dummyTask, "ThA"); ThA.setPriority(1); ThA.start();
-
-        Thread Th1_G3 = new Thread(G3, dummyTask, "Th1_G3"); Th1_G3.setPriority(4); Th1_G3.start();
-        Thread Th2_G3 = new Thread(G3, dummyTask, "Th2_G3"); Th2_G3.setPriority(3); Th2_G3.start();
-        Thread Th3_G3 = new Thread(G3, dummyTask, "Th3_G3"); Th3_G3.setPriority(5); Th3_G3.start();
-
-        Thread Th1_main = new Thread(dummyTask, "Th1_main"); Th1_main.setPriority(3); Th1_main.start();
-        Thread Th2_main = new Thread(dummyTask, "Th2_main"); Th2_main.setPriority(6); Th2_main.start();
-
-
+    public void run() {
+        System.out.println(
+                "Thread '" + getName() +
+                        "' from Group '" + getThreadGroup().getName() +
+                        "' started with priority " + getPriority()
+        );
         try {
             Thread.sleep(100);
-        } catch (InterruptedException e) {}
-
-
-        System.out.println("\n=== ПЕРЕЧИСЛЕНИЕ ВСЕЙ СТРУКТУРЫ ПОТОКОВ ===");
-
-        ThreadGroup rootGroup = mainGroup.getParent();
-        if (rootGroup == null) rootGroup = mainGroup;
-
-        listGroup(rootGroup, "");
-
-
-        try {
-            Thread[] allThreads = {Tha, Thb, Thc, Thd, ThA, Th1_G3, Th2_G3, Th3_G3, Th1_main, Th2_main};
-            for (Thread t : allThreads) {
-                t.join(10);
-            }
-        } catch (InterruptedException ignored) {
-            Thread.currentThread().interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
+}
 
-    public static void listGroup(ThreadGroup g, String indent) {
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        ThreadGroup main = new ThreadGroup("Main");
+        ThreadGroup g2 = new ThreadGroup(main, "G2");
+        ThreadGroup g4 = new ThreadGroup(g2, "G4");
+        ThreadGroup g1 = new ThreadGroup(g4, "G1");
+        ThreadGroup g3 = new ThreadGroup(main, "G3");
 
-        System.out.println(indent + "GROUP: " + g.getName() +
-                " [Max Pri: " + g.getMaxPriority() + "]");
+        /* G1 group */
+        Thread Tha = new Custom_thread(g1, "Tha", 1);
+        Thread Thb = new Custom_thread(g1, "Thb", 3);
+        Thread Thc = new Custom_thread(g1, "Thc", 8);
+        Thread Thd = new Custom_thread(g1, "Thd", 3);
 
-        int numThreads = g.activeCount();
-        Thread[] threads = new Thread[numThreads * 2];
-        numThreads = g.enumerate(threads, false);
+        /* G4 group */
+        Thread ThA = new Custom_thread(g4, "ThA", 1);
 
-        int numGroups = g.activeGroupCount();
-        ThreadGroup[] groups = new ThreadGroup[numGroups * 2];
-        numGroups = g.enumerate(groups, false);
+        /* G3 group */
+        Thread Th1_g3 = new Custom_thread(g3, "Th1", 4);
+        Thread Th2_g3 = new Custom_thread(g3, "Th2", 3);
+        Thread Th3_g3 = new Custom_thread(g3, "Th3", 5);
 
-        String threadIndent = indent + "  ";
-        String groupIndent = indent + "  ";
+        /* Main group */
+        Thread Th1 = new Custom_thread(main, "Th1", 3);
+        Thread Th2 = new Custom_thread(main, "Th2", 6);
 
-        for (int i = 0; i < numThreads; i++) {
-            Thread t = threads[i];
+        Tha.start();
+        Thb.start();
+        Thc.start();
+        Thd.start();
+        ThA.start();
+        Th1_g3.start();
+        Th2_g3.start();
+        Th3_g3.start();
+        Th1.start();
+        Th2.start();
 
-            if (t != null && !t.isDaemon()) {
-                System.out.println(threadIndent +
-                        "Th: " + t.getName() +
-                        ", pri=" + t.getPriority() +
-                        ", state=" + t.getState());
-            }
-        }
+        Tha.join();
+        Thb.join();
+        Thc.join();
+        Thd.join();
+        ThA.join();
+        Th1_g3.join();
+        Th2_g3.join();
+        Th3_g3.join();
+        Th1.join();
+        Th2.join();
 
-        // Рекурсивный вызов для дочерних групп
-        for (int i = 0; i < numGroups; i++) {
-            if (groups[i] != null) {
-                listGroup(groups[i], groupIndent);
-            }
-        }
+        main.list();
+        System.out.println("Total active threads is " + main.activeCount());
     }
 }
