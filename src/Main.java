db66bde6767aacca1681d8b8e8e9327c5fdb5bcf
Nichita Stepanovici <nichita.stepanovici@iis.utm.md>
2025-11-05 19:23:08 +0200

    peredelal zapusk potokov i vivod texta

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
-
-
 public class Main {
 
     static int[] numbers = new int[50];
+
+
     static final Object lock1 = new Object();
-    static final Object lock2 = new Object();
     static boolean firstDone = false;
+
+
+    static final Object lock2 = new Object();
     static boolean thirdDone = false;
+    static boolean fourthDone = false;
+
+    static final Object printLock = new Object();
 
     public static void main(String[] args) {
 
-        // 1. Заполняем массив
+
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = (int) (Math.random() * 100);
         }
@@ -19,42 +24,57 @@ public class Main {
         for (int n : numbers) System.out.print(n + " ");
         System.out.println("\n");
 
-        // 2. Создаём потоки
-        SumEvenPositionsForward t1 = new SumEvenPositionsForward();
-        SumEvenPositionsBackward t2 = new SumEvenPositionsBackward();
-        ForwardInterval t3 = new ForwardInterval();
-        ReverseInterval t4 = new ReverseInterval();
 
-        // 3. Запускаем их как обычные методы
-        t1.run();
-        t2.run();
-        t3.run();
-        t4.run();
+        Thread t1 = new Thread(new SumEvenPositionsForward());
+        Thread t2 = new Thread(new SumEvenPositionsBackward());
+        Thread t3 = new Thread(new ForwardInterval());
+        Thread t4 = new Thread(new ReverseInterval());
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
     }
 
-    // Поток 1: сумма позиций чётных чисел с начала
+
     static class SumEvenPositionsForward implements Runnable {
         @Override
         public void run() {
-            int sum = 0;
-            System.out.println("Поток 1: Сумма позиций чётных чисел (вперёд)");
-            for (int i = 0; i < numbers.length; i++) {
-                if (numbers[i] % 2 == 0) {
-                    sum += i;
-                }
-            }
-            System.out.println("Результат потока 1: " + sum);
+            System.out.println("Поток 1: Сумма позиций чётных чисел по два");
 
+            int i = 0;
+            while (i < numbers.length - 1) {
+
+                if (numbers[i] % 2 == 0) {
+                    int pos1 = i;
+                    int firstEven = numbers[i];
+                    i++;
+
+
+                    while (i < numbers.length && numbers[i] % 2 != 0) {
+                        i++;
+                    }
+
+                    if (i < numbers.length) {
+                        int pos2 = i;
+                        int secondEven = numbers[i];
+                        int sumPositions = pos1 + pos2;
+                        System.out.println("Поток 1: числа[" + pos1 + "]=" + firstEven +
+                                " + числа[" + pos2 + "]=" + secondEven +
+                                " => позиции: " + pos1 + "+" + pos2 + "=" + sumPositions);
+                    }
+                }
+                i++;
+            }
             synchronized (lock1) {
                 firstDone = true;
-                lock1.notify(); // начало 2 потока
+                lock1.notify();
             }
 
             printText("Фамилия: Stepanovici", 100);
         }
     }
 
-    // Поток 2: сумма позиций чётных чисел с конца
     static class SumEvenPositionsBackward implements Runnable {
         @Override
         public void run() {
@@ -62,74 +82,107 @@ public class Main {
                 while (!firstDone) {
                     try {
                         lock1.wait();
-                    } catch (InterruptedException ignored) {}
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
 
-            int sum = 0;
-            System.out.println("Поток 2: Сумма позиций чётных чисел (назад)");
-            for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.println("\nПоток 2: Сумма позиций чётных чисел по два с конца");
+
+            int i = numbers.length - 1;
+            while (i > 0) {
                 if (numbers[i] % 2 == 0) {
-                    sum += i;
-                }
-            }
-            System.out.println("Результат потока 2: " + sum);
+                    int pos1 = i;
+                    int firstEven = numbers[i];
+                    i--;
 
-            printText("Имя: Nichita", 100);
+                    while (i >= 0 && numbers[i] % 2 != 0) {
+                        i--;
+                    }
+
+                    if (i >= 0) {
+                        int pos2 = i;
+                        int secondEven = numbers[i];
+                        int sumPositions = pos1 + pos2;
+                        System.out.println("Поток 2: числа[" + pos1 + "]=" + firstEven +
+                                " + числа[" + pos2 + "]=" + secondEven +
+                                " => позиции: " + pos1 + "+" + pos2 + "=" + sumPositions);
+                    }
+                }
+                i--;
+            }
+
+            printText("\nИмя: Nichita", 100);
         }
     }
 
-    // Поток 3 интервал [120, 690]
     static class ForwardInterval implements Runnable {
         @Override
         public void run() {
-            System.out.println("Поток 3: Интервал [120, 690]");
-            for (int i = 120; i <= 690; i += 100) {
+            System.out.println("\nПоток 3: Интервал [120, 690]");
+            for (int i = 120; i <= 690; i += 10) {
                 System.out.print(i + " ");
+                if ((i - 120) % 100 == 90) System.out.println();
             }
             System.out.println();
 
             synchronized (lock2) {
                 thirdDone = true;
-                lock2.notify(); // разрешаем потоку 4
+                lock2.notifyAll();
+
+                while (!fourthDone) {
+                    try {
+                        lock2.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
 
-            printText("Дисциплина: Конкурентное и распределённое программирование", 100);
+            printText("\nДисциплина: Конкурентное программирование", 100);
         }
     }
 
-    // Поток 4 интервал [1000, 1567] с конца
     static class ReverseInterval implements Runnable {
         @Override
         public void run() {
             synchronized (lock2) {
-                while (!thirdDone) { // ждём поток 3
+                while (!thirdDone) {
                     try {
                         lock2.wait();
-                    } catch (InterruptedException ignored) {}
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
 
-            System.out.println("Поток 4: Интервал [1000, 1567] (обратно)");
-            for (int i = 1567; i >= 1000; i -= 100) {
+            System.out.println("\nПоток 4: Интервал [1000, 1567] ");
+            for (int i = 1567; i >= 1000; i -= 10) {
                 System.out.print(i + " ");
+                if ((1567 - i) % 100 == 90) System.out.println();
             }
             System.out.println();
 
-            printText("Группа: Cr-233", 100);
+            synchronized (lock2) {
+                fourthDone = true;
+                lock2.notifyAll();
+            }
+
+            printText("\nГруппа: Cr-233", 100);
         }
     }
 
-    // Плавный вывод текста
     static void printText(String text, int delayMillis) {
-        long delay = delayMillis * 1_000_000L;
-        for (char c : text.toCharArray()) {
-            System.out.print(c);
-            long start = System.nanoTime();
-            while (System.nanoTime() - start < delay) {
-
+        synchronized (printLock) {
+            long delay = delayMillis * 1_000_000L;
+            for (char c : text.toCharArray()) {
+                System.out.print(c);
+                long start = System.nanoTime();
+                while (System.nanoTime() - start < delay) {
+                }
             }
+            System.out.println();
         }
-        System.out.println();
     }
 }
